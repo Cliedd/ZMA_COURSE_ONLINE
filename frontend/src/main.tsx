052430237f495d2e10c6d4a58d60896361a99a79
Ryzen237 <boumbisaiitiel@gmail.com>
2026-08-03 02:55:56 +0100

    chore: clean up netlify.toml ignore-command test artifacts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-// no-op comment: verifying netlify.toml `ignore` doesn't block real frontend deploys
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
